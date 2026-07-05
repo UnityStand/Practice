d2267ee49254d11740d979b97369d04f88a8e0cc
zubzov <zubzov@bfs-it.ru>
2026-07-05 18:36:54 +0300

    Заполнил readme. Поправил поле класса  Event.Title

--- ASP.NET Core Web API/Models/Event.cs
+++ ASP.NET Core Web API/Models/Event.cs
@@ -3,7 +3,7 @@
 public class  Event
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;    
     public string? Description { get; set; }
     public DateTime StartAt { get; set; }
     public DateTime EndAt { get; set; }
