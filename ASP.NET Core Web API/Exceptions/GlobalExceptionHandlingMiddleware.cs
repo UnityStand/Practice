5b532373da5ae8f616116c1fee49188cad5a8a5e
zubzov <zubzov@bfs-it.ru>
2026-07-12 22:46:42 +0300

    Убрал лишниее переменные (спасибо Rider) . Спрятал стактрейса для 500

--- ASP.NET Core Web API/Exceptions/GlobalExceptionHandlingMiddleware.cs
+++ ASP.NET Core Web API/Exceptions/GlobalExceptionHandlingMiddleware.cs
@@ -23,15 +23,17 @@ namespace ASP.NET_Core_Web_API.Exceptions;
           // 2. определить статус-код в зависимости от типа exception
           var statusCode = exception switch
           {
-              ValidationException ve => StatusCodes.Status400BadRequest,
-              NotFoundException nfe => StatusCodes.Status404NotFound,
+              ValidationException => StatusCodes.Status400BadRequest,
+              NotFoundException => StatusCodes.Status404NotFound,
 
               _ => StatusCodes.Status500InternalServerError                                                                                   
           };
           //  3. собрать ProblemDetails (Status, Title/Detail)
           var error= new ProblemDetails{
-              Status = statusCode,
-              Detail = exception.Message};
+              Status = statusCode, 
+              Detail = statusCode == StatusCodes.Status500InternalServerError? 
+                  "Произошла непредвиденная ошибка"
+                  : exception.Message};
           //  4. httpContext.Response.StatusCode = ...
           httpContext.Response.StatusCode = statusCode;           
           //  5. записать тело ответа, например через httpContext.Response.WriteAsJsonAsync(...)
