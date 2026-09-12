5d8e1212d31db0c4f5599f4825e69059fb38726a
zubzov <zubzov@bfs-it.ru>
2026-09-12 15:02:03 +0300

    Тесты для этапа 5

--- tests/ASP.NET Core Web API.Tests/BookingServiceTest.cs
+++ tests/ASP.NET Core Web API.Tests/BookingServiceTest.cs
@@ -18,6 +18,8 @@ public class BookingServiceTests : IDisposable
         var dbName = Guid.NewGuid().ToString();
         var services = new ServiceCollection();
         services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase(dbName));
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
         services.AddScoped<IEventService, EventService>();
         services.AddScoped<IBookingService, BookingService>();
         _serviceProvider = services.BuildServiceProvider();
