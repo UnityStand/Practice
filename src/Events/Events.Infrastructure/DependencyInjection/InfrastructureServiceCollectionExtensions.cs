499ac3208ab5640db058c639bcc828b539a4bd72
zubzov <zubzov@bfs-it.ru>
2026-09-24 21:00:17 +0300

    Этап 2. Events: StackExchange.Redis, RedisOptions, IConnectionMultiplexer в DI

--- src/Events/Events.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
+++ src/Events/Events.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
-﻿using Events.Application.Abstractions;  
+﻿using Events.Application.Abstractions;
+using Events.Infrastructure.Caching;
 using Events.Infrastructure.Messaging;
 using Events.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 
 namespace Events.Infrastructure.DependencyInjection;
 
@@ -13,12 +16,19 @@ public static class InfrastructureServiceCollectionExtensions
         IConfiguration configuration)
     {
         services.AddDbContext<EventDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-
-        services.Configure<KafkaOptions>(configuration.GetSection("Kafka"));                                                               
+        services.Configure<KafkaOptions>(configuration.GetSection("Kafka"));        
+        services.Configure<RedisOptions>(configuration.GetSection("Redis"));       
         services.AddHostedService<KafkaTopicInitializer>(); 
         services.AddScoped<IEventRepository, EventRepository>();
         services.AddScoped<IProcessedBookingRepository, ProcessedBookingRepository>();
-        services.AddHostedService<BookingConfirmedConsumer>();  
+        services.AddHostedService<BookingConfirmedConsumer>();
+        services.AddSingleton<IConnectionMultiplexer>(sp =>                                                                                
+        {                                                                                                                                  
+            var redis = sp.GetRequiredService<IOptions<RedisOptions>>().Value;                                                             
+            var options = ConfigurationOptions.Parse(redis.ConnectionString);  
+            options.AbortOnConnectFail = false;
+            return ConnectionMultiplexer.Connect(options);                                                                                 
+        });      
         return services;
     }
 }
